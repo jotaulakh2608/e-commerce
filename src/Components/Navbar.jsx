f1643038be0a05cfddf945b2698308c8f41eb1a4
Love Aulakh <rattanjotsingh02@gmail.com>
2022-08-08 17:26:51 +0530

    final

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
   <div class="container flex flex-wrap justify-between items-center mx-auto">
   <span onClick={()=>navigate('/cart')}  class="flex items-center">
       <img src="https://www.pinpng.com/pngs/m/582-5827885_sikh-cartoon-hd-png-download.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">E-Punjabi</span>
+      <span class="self-center text-xl font-semibold whitespace-nowrap text-white">E-Punjabi</span>
   </span>
   <div class="flex md:order-2">
   <div className='text-3xl text-white cursor-pointer' onClick={()=>dispatch(showCart())}  >
@@ -26,7 +26,7 @@ export const Navbar = () => {
     
   </div>
   <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    <ul class="flex flex-col p-4 mt-4  rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
         <a href="/" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
       </li>
